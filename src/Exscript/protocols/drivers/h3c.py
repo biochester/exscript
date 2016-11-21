efb4f90e60cdde4da8f045e375ba121b569e2997
biochester <chezzz@xaker.ru>
2016-11-21 14:21:10 +0700

    Update h3c.py

--- src/Exscript/protocols/drivers/h3c.py
+++ src/Exscript/protocols/drivers/h3c.py
@@ -14,6 +14,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 """
 A driver for devices running Comware Software (by Huawei and 3Com)
+based on vrp driver
 .
 """
 import re
